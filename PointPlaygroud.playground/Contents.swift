75001477e5cb5fcb5ad3734bf355eaea0bf869b9
Hai Feng Kao <haifeng@cocoaspice.in>
2021-12-09 21:22:58 +0800

    update playground

--- PointPlaygroud.playground/Contents.swift
+++ PointPlaygroud.playground/Contents.swift
@@ -1,15 +1,19 @@
 import UIKit
 
 struct Point {
-    let x: Float
-    let y: Float
+    var x: Float
+    var y: Float
 }
 
 let p1: Point = .init(x: 1.0, y: 2.0)
 let p2: Point = .init(x: 3.0, y: 4.0)
+let p3: Point = .init(x: 5.0, y: 6.0)
 
-let sum: Point = .init(x: p1.x + p2.x,
-                       y: p1.y + p2.y)
+var sum: Point = .init(x: 0.0, y: 0.0)
+for p in [p1, p2, p3] {
+    sum.x += p.x
+    sum.y += p.y
+}
 
 print("sum:", sum.x, sum.y)
 
@@ -20,5 +24,5 @@ struct ComposedPoint {
     var y: Float { points.map(\.y).reduce(0.0, +) }
 }
 
-let sum2 = ComposedPoint(points: [p1, p2])
+let sum2 = ComposedPoint(points: [p1, p2, p3])
 print("sum2:", sum2.x, sum2.y)
